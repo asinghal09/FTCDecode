90b1e537710dcfb1d4d83b47f56d19530a83ebdb
asinghal09 <94330788+asinghal09@users.noreply.github.com>
2025-10-28 21:07:12 -0400

    tuned roadrunner + part ish of red goal auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/transfer/Auto/subsystems/Intake.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.transfer.Auto.subsystems;
+
+import com.acmerobotics.roadrunner.Action;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Intake {
+    private DcMotorEx intake;
+    public static double intakePower = -1;
+
+    public Intake(HardwareMap hardwareMap) {
+        intake  = hardwareMap.get(DcMotorEx.class, "intake");
+        intake.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.FLOAT);
+    }
+
+
+    public Action intakeOn() {
+        return t -> {
+            intake.setPower(intakePower);
+            return false;
+        };
+    }
+    public Action intakeOff() {
+        return t -> {
+            intake.setPower(0);
+            return false;
+        };
+    }
+}
