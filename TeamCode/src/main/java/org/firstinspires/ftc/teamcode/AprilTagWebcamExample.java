12b01303b39b7bbf3d8b6d3e4c0c491e016a50a8
ykumar27 <emailyk02@gmail.com>
2025-11-11 20:09:43 -0500

    camera stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagWebcamExample.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+
+@Autonomous
+public class AprilTagWebcamExample extends OpMode{
+
+    AprilTagWebcam aprilTagWebcam = new AprilTagWebcam();
+
+    @Override
+    public void init() {
+        aprilTagWebcam.init(hardwareMap, telemetry);
+    }
+
+    @Override
+    public void loop() {
+        aprilTagWebcam.update();
+        AprilTagDetection id20 = aprilTagWebcam.getTagBySpecificId(20);
+
+        if (id20 != null) {
+            aprilTagWebcam.displayDetectionTelemetry(id20);
+        } else {
+            telemetry.addLine("Tag ID 20 not detected");
+        }
+
+        telemetry.update();
+    }
+}
